ad279b7dafd14bb535f24796f3a8d9cd2e93e53b
Daniel Thom <daniel.thom@nrel.gov>
2019-12-18 02:42:08 -0700

    Remove globals from unit test

--- tests/unit/jobs/test_results_aggregator.py
+++ tests/unit/jobs/test_results_aggregator.py
@@ -17,6 +17,8 @@ OUTPUT = os.path.join(tempfile.gettempdir(), "results-aggregator-output")
 
 @pytest.fixture
 def cleanup():
+    pytest.aggregator1 = None
+    pytest.aggregator2 = None
     if os.path.exists(OUTPUT):
         shutil.rmtree(OUTPUT)
 
@@ -26,20 +28,15 @@ def create_result(index):
     return Result(str(index), index, "finished", 1.0 + index)
 
 
-aggregator1 = None
-aggregator2 = None
-
-
 def append(result):
     if int(result.name) % 2 == 0:
-        aggregator1.append_result(result)
+        pytest.aggregator1.append_result(result)
     else:
-        aggregator2.append_result(result)
+        pytest.aggregator2.append_result(result)
 
 
 def test_results_aggregator(cleanup):
     """Test ResultsAggregator"""
-    global aggregator1, aggregator2
     if os.path.exists(OUTPUT):
         shutil.rmtree(OUTPUT)
     os.makedirs(os.path.join(OUTPUT, RESULTS_DIR))
@@ -47,19 +44,19 @@ def test_results_aggregator(cleanup):
     results = [create_result(i) for i in range(100)]
     batch1_file = get_results_temp_filename(OUTPUT, 1)
     batch2_file = get_results_temp_filename(OUTPUT, 2)
-    aggregator1 = ResultsAggregator(batch1_file)
-    aggregator2 = ResultsAggregator(batch2_file)
-    aggregator1.create_file()
-    aggregator2.create_file()
-    assert os.path.exists(aggregator1._filename)
-    assert os.path.exists(aggregator2._filename)
+    pytest.aggregator1 = ResultsAggregator(batch1_file)
+    pytest.aggregator2 = ResultsAggregator(batch2_file)
+    pytest.aggregator1.create_file()
+    pytest.aggregator2.create_file()
+    assert os.path.exists(pytest.aggregator1._filename)
+    assert os.path.exists(pytest.aggregator2._filename)
 
     with ProcessPoolExecutor() as executor:
         executor.map(append, results)
 
-    final_results1 = aggregator1.get_results()
+    final_results1 = pytest.aggregator1.get_results()
     final_results1.sort(key=lambda x: int(x.name))
-    final_results2 = aggregator2.get_results()
+    final_results2 = pytest.aggregator2.get_results()
     final_results2.sort(key=lambda x: int(x.name))
 
     expected1 = [x for x in results if int(x.name) % 2 == 0]
