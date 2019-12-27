b79c0dbbfa7469b193456c377ab5293c657a13ea
Jeremy Jenkins <jjenkins@nrel.gov>
2019-12-27 12:20:54 -0700

    test(job-config-factory): add tests for loading config from results

---
+++ tests/unit/jobs/test_job_configuration_factory.py
@@ -0,0 +1,93 @@
+"""
+Unit tests for JobQueue class
+"""
+import mock
+import os
+import pytest
+
+from jade.exceptions import InvalidParameter
+from jade.jobs.job_configuration_factory import (
+    create_config_from_file,
+    create_config_from_previous_run
+)
+from jade.result import Result, ResultsSummary, serialize_results
+
+TEST_DATA_DIR = "tests/data/demo"
+CONFIG_FILE = os.path.join(TEST_DATA_DIR, "test-config.json")
+OUTPUT_DIRECTORY = os.path.join(TEST_DATA_DIR, "output")
+
+@pytest.fixture
+def jade_results():
+    """Fixture of jade results"""
+    return [
+        Result("australia", 1, "unfinished", 10, 15555555555),
+        Result("brazil", 0, "finished", 20, 15555555555),
+        Result("united_states", 0, "finished", 30, 15555555555),
+    ]
+
+@pytest.fixture
+def jade_data():
+    """Fixture of serialized jade result"""
+    return {
+        "base_directory": "/jade/results/base/directory/",
+        "results": [
+            {
+                "name": "australia",
+                "return_code": 1,
+                "status": "unfinished",
+                "exec_time_s": 10,
+                "completion_time": 15555555555
+            },
+            {
+                "name": "brazil",
+                "return_code": 0,
+                "status": "finished",
+                "exec_time_s": 20,
+                "completion_time": 15555555555
+            },
+            {
+                "name": "united_states",
+                "return_code": 0,
+                "status": "finished",
+                "exec_time_s": 30,
+                "completion_time": 15555555555
+            },
+        ],
+        "jade_version": 0.1,
+        "timestamp": "2019-09-02 15:00:00"
+    }
+
+@pytest.fixture
+def results_summary(jade_data, jade_results):
+    """Fixture of ResultsSummary instance"""
+    ResultsSummary._parse = mock.MagicMock(return_value=jade_data)
+
+def test_create_config_from_file():
+    """Create should successfully return config"""
+    config = create_config_from_file(CONFIG_FILE)
+    assert len(config.list_jobs()) == 3
+
+def test_create_config_from_file_missing_file():
+    """Create should throw FileNotFoundError"""
+    with pytest.raises(FileNotFoundError):
+        create_config_from_file("a" + CONFIG_FILE)
+
+def test_create_config_from_previous_run_successful_results(results_summary):
+    """Create should return config with 2 jobs"""
+    successful_config = create_config_from_previous_run(CONFIG_FILE, OUTPUT_DIRECTORY)
+    assert len(successful_config.list_jobs()) == 2
+    for job in successful_config.list_jobs():
+        assert job.name in [ "brazil", "united_states" ]
+
+def test_create_config_from_previous_run_failed_results(results_summary):
+    """Create should return config with 1 job"""
+    failed_config = create_config_from_previous_run(CONFIG_FILE, OUTPUT_DIRECTORY, "failed")
+    assert len(failed_config.list_jobs()) == 1
+    for job in failed_config.list_jobs():
+        assert job.name in [ "australia" ]
+
+@pytest.mark.noautofixt
+def test_create_config_from_previous_run_invalid_type_results(results_summary):
+    """Create should throw InvalidParameter"""
+    with pytest.raises(InvalidParameter):
+        create_config_from_previous_run(CONFIG_FILE, OUTPUT_DIRECTORY, "invalid_type")
