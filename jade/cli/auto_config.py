9ed980946bf8a3d863633049082c448d3f600212
Jeremy Jenkins <jjenkins@nrel.gov>
2020-01-23 11:21:38 -0700

    improvement(auto-config): check if post-process config file given and load into job config

--- jade/cli/auto_config.py
+++ jade/cli/auto_config.py
@@ -7,6 +7,8 @@ from jade.common import CONFIG_FILE
 from jade.loggers import setup_logging
 from jade.exceptions import InvalidExtension
 from jade.extensions.registry import Registry, ExtensionClassType
+from jade.jobs.job_post_process import JobPostProcess
+from jade.utils.utils import load_data
 
 
 # TODO: need one group command for auto-config; this should be a subcommand.
@@ -25,13 +27,31 @@ from jade.extensions.registry import Registry, ExtensionClassType
     show_default=True,
     help="config file to generate."
 )
+@click.option(
+    "-p", "--post-process-config-file",
+    is_eager=True,
+    help="TOML file containing overrides specific to analysis type"
+)
 @click.argument("inputs")
 @click.argument("extension")
 @click.command()
-def auto_config(extension, inputs, config_file, verbose):
+def auto_config(extension, inputs, config_file, post_process_config_file,
+                verbose):
     """Automatically create a configuration."""
     level = logging.DEBUG if verbose else logging.WARNING
     setup_logging("auto_config", None, console_level=level)
+
+    post_process_config = None
+    if post_process_config_file is not None:
+        module, class_name, data = JobPostProcess.load_config_from_file(post_process_config_file)
+        # ensure everything ok
+        JobPostProcess(module, class_name, data)
+
+        post_process_config = {
+            "module": module,
+            "class_name": class_name,
+            "data": data
+        }
 
     # User extension
     registry = Registry()
@@ -39,7 +59,7 @@ def auto_config(extension, inputs, config_file, verbose):
         raise InvalidExtension(f"Extension '{extension}' is not registered.")
 
     cli = registry.get_extension_class(extension, ExtensionClassType.CLI)
-    config = cli.auto_config(inputs)
+    config = cli.auto_config(inputs, post_process_config)
     print(f"Created configuration with {config.get_num_jobs()} jobs.")
     config.dump(config_file)
     print(f"Dumped configuration to {config_file}.")
