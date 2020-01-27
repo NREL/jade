65f8400ad364c3453cd03ea46b38f61c6903740b
Jeremy Jenkins <jjenkins@nrel.gov>
2020-01-27 15:20:16 -0700

    feat(show-results): add flags for printing post-process results for all/specific jobs

--- jade/jobs/job_post_process.py
+++ jade/jobs/job_post_process.py
@@ -6,14 +6,19 @@ import logging
 import json
 import toml
 
+from prettytable import PrettyTable
+
+from jade.common import JOBS_OUTPUT_DIR
 from jade.utils.utils import load_data, output_to_file
 
 logger = logging.getLogger(__name__)
 
 class JobPostProcess:
     """Class used to dynamically run post process scripts"""
+    _output_dir = f"output/{JOBS_OUTPUT_DIR}"
+    _results_file = "post-process-results.json"
 
-    def __init__(self, module_name, class_name, data=None, **kwargs):
+    def __init__(self, module_name, class_name, data=None, job_name=None, **kwargs):
         """Constructs JobPostProcess
 
         Parameters
@@ -24,12 +29,13 @@ class JobPostProcess:
 
         """
         self._data = data
+        self._job_name = job_name
 
         try:
             # dynamically get class from analysis module
             process_module = __import__(module_name, fromlist=[class_name])
             process_class = getattr(process_module, class_name)
-            self._post_process = process_class(self._data, **kwargs)
+            self._post_process = process_class(self._data, self._job_name, **kwargs)
         except ModuleNotFoundError as module_error:
             logger.exception(module_error)
         except ValueError as value_error:
@@ -71,6 +77,7 @@ class JobPostProcess:
     def run(self, *kwargs):
         """Runs post-process class' run function"""
         self._post_process.run(*kwargs)
+        self._dump_results()
 
     def serialize(self):
         """Create data for serialization."""
@@ -84,7 +91,47 @@ class JobPostProcess:
 
         return serialized_data
 
+    @classmethod
+    def show_results(self, job_name=None):
+        """Show the post process results for jobs in a table.
+
+        Parameters
+        ----------
+        job_name : str
+            optional individual job to display
+        """
+
+        print(f"Post-process results from directory: {self._output_dir}")
+
+        job_names = None
+        for _, dirs, _ in os.walk(self._output_dir):
+            job_names = dirs
+            break
+
+        table = PrettyTable()
+
+        for job in job_names:
+            # skip job if not given job name
+            if job_name is not None and job_name != job:
+                continue
+
+            results = load_data(f"{self._output_dir}/{job}/{self._results_file}")
+
+            if not table.field_names:
+                table.field_names = [ 'job' ] + list(results[0].keys())
+
+            for result in results:
+                row = [ job ]
+                for column in result:
+                    row.append(result[column])
+
+                table.add_row(row)
+
+        print(table)
+
+    def _dump_results(self):
+        results = self._post_process.get_results()
+        output_to_file(results, f"{self._output_dir}/{self._job_name}/{self._results_file}")
+
     def dump(self, filename=None):
-        if filename is None:
-            filename = "post-process-config.toml"
-        output_to_file(self.serialize(), filename)
+        output_to_file(self.serialize(), self._results_file)
