a404cda92d125366f8f5421cd519d7131ed8d18f
jgu2 <jianli.gu@nrel.gov>
2020-12-04 13:35:27 -0700

    Add example_output fixture

--- tests/integration/cli/test_stats.py
+++ tests/integration/cli/test_stats.py
@@ -2,12 +2,9 @@
 from jade.utils.subprocess_manager import run_command
 
 
-OUTPUT_DIR = "tests/data/example_output"
-
-
-def test_stats__show():
+def test_stats__show(example_output):
     output = {}
-    ret = run_command(f"jade stats show -o {OUTPUT_DIR}", output)
+    ret = run_command(f"jade stats show -o {example_output}", output)
     assert ret == 0
     assert len(output["stdout"]) > 0
 
@@ -15,25 +12,25 @@ def test_stats__show():
         assert text in output["stdout"]
 
 
-def test_stats__bytes_consumed():
+def test_stats__bytes_consumed(example_output):
     output = {}
-    ret = run_command(f"jade stats bytes-consumed -o {OUTPUT_DIR}", output)
+    ret = run_command(f"jade stats bytes-consumed -o {example_output}", output)
     assert ret == 0
     assert len(output["stdout"]) > 0
 
-    ret = run_command(f"jade stats bytes-consumed --no-human-readable -o {OUTPUT_DIR}", output)
+    ret = run_command(f"jade stats bytes-consumed --no-human-readable -o {example_output}", output)
     assert ret == 0
     bytes_consumed = int(output["stdout"].strip())
     assert bytes_consumed > 0
 
 
-def test_stats__exec_time():
+def test_stats__exec_time(example_output):
     output = {}
-    ret = run_command(f"jade stats exec-time -o {OUTPUT_DIR}", output)
+    ret = run_command(f"jade stats exec-time -o {example_output}", output)
     assert ret == 0
     assert len(output["stdout"]) > 0
 
-    ret = run_command(f"jade stats exec-time --no-human-readable -o {OUTPUT_DIR}", output)
+    ret = run_command(f"jade stats exec-time --no-human-readable -o {example_output}", output)
     assert ret == 0
     exec_time = float(output["stdout"].strip())
     assert exec_time > 0
