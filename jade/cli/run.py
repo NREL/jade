3d63e171a433878f48f29abf3175af649bde56dc
jgu2 <jianli.gu@nrel.gov>
2020-12-23 17:47:29 -0700

    Remove a comma

--- jade/cli/run.py
+++ jade/cli/run.py
@@ -101,7 +101,7 @@ def run(extension, **kwargs):
                     class_name=config['job_post_process_config']['class'],
                     data=config['job_post_process_config']['data'],
                     job_name=name,
-                    output=output,
+                    output=output
                 )
                 post_process.run(config_file=config_file, output=output)
         except Exception as err:
