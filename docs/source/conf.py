33002cfbe760724a9a8f9475fc257673a8c379fd
Daniel Thom <daniel.thom@nrel.gov>
2021-06-02 16:47:41 -0600

    Bump version to v0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, NREL"
 author = "NREL"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.0"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
