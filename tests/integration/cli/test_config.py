e2705053f44b6007e250507966cbe411f1d7bf25
Daniel Thom <daniel.thom@nrel.gov>
2022-04-11 16:41:26 -0600

    Add assign-blocked-by command

--- tests/integration/cli/test_config.py
+++ tests/integration/cli/test_config.py
@@ -4,6 +4,7 @@ import sys
 
 import pytest
 
+from jade.extensions.generic_command import GenericCommandConfiguration, GenericCommandParameters
 from jade.utils.subprocess_manager import run_command
 from jade.utils.utils import load_data
 
@@ -68,7 +69,7 @@ def test_config__filter_range(cleanup):
     assert ret == 0
     assert os.path.exists(CONFIG1)
 
-    ret = run_command(f"jade config filter {CONFIG1} -o {CONFIG2} :2")
+    ret = run_command(f"jade config filter {CONFIG1} -o {CONFIG2} 0 1")
     assert ret == 0
     assert os.path.exists(CONFIG2)
 
@@ -102,3 +103,52 @@ def test_config__filter_show_only(cleanup):
     assert not os.path.exists(CONFIG2)
 
     assert "brazil" in output["stdout"]
+
+
+def test_config__assign_blocked_by(cleanup):
+    config = GenericCommandConfiguration()
+    base_cmd = "bash my_script.sh"
+    regular_job_names = []
+    for i in range(1, 4):
+        cmd = base_cmd + " " + str(i)
+        name = f"job_{i}"
+        job = GenericCommandParameters(
+            command=cmd,
+            name=name,
+            append_output_dir=True,
+        )
+        config.add_job(job)
+        regular_job_names.append(name)
+
+    pp_name = "post_process"
+    post_process_job = GenericCommandParameters(
+        command="bash run_post_process.sh",
+        name=pp_name,
+        append_output_dir=True,
+    )
+    config.add_job(post_process_job)
+    config_file = CONFIG1
+    config.dump(config_file, indent=2)
+
+    ret = run_command(f"jade config assign-blocked-by {CONFIG1} 3 -o {CONFIG2}")
+    assert ret == 0
+    assert os.path.exists(CONFIG2)
+
+    config = load_data(CONFIG2)
+    assert sorted(config["jobs"][3]["blocked_by"]) == sorted(regular_job_names)
+
+    os.remove(CONFIG2)
+    ret = run_command(f"jade config assign-blocked-by {CONFIG1} 3 1 2 -o {CONFIG2}")
+    assert ret == 0
+    assert os.path.exists(CONFIG2)
+    config = load_data(CONFIG2)
+    expected = [regular_job_names[1], regular_job_names[2]]
+    assert sorted(config["jobs"][3]["blocked_by"]) == sorted(expected)
+
+    # Include the pp job in blocking-job-indexes.
+    ret = run_command(f"jade config assign-blocked-by {CONFIG1} 3 1 2 3 -o {CONFIG2}")
+    assert ret != 0
+
+    # Invalid job index
+    ret = run_command(f"jade config assign-blocked-by {CONFIG1} 47 1 2 -o {CONFIG2}")
+    assert ret != 0
