4f8fce3bae582003e3758cdc5b0f60baf029496b
Daniel Thom <daniel.thom@nrel.gov>
2022-12-01 09:25:10 -0700

    Make process table column names consistent

---
+++ tests/integration/test_resource_stats.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+from jade.utils.run_command import check_run_command
+
+
+SUBMIT_JOBS = "jade submit-jobs -f -l -m cpu -m memory -m process -m disk -m network"
+
+
+def test_resource_stats(temporary_directory):
+    script = temporary_directory / "sleep.sh"
+    script.write_text("sleep 3\n")
+    commands_file = temporary_directory / "commands.txt"
+    commands_file.write_text(f"bash {script}\n")
+    config_file = temporary_directory / "config.json"
+    check_run_command(f"jade config create {commands_file} -c {config_file}")
+
+    for mode in ("aggregation", "periodic"):
+        output_dir = temporary_directory / "output-dir"
+        cmd = f"{SUBMIT_JOBS} {config_file} -p 1 -R {mode} -r 1 -o {output_dir}"
+        check_run_command(cmd)
+        db_file = output_dir / "results.db"
+        files = [
+            db_file,
+            output_dir / "stats_summary.json",
+        ]
+        if mode == "periodic":
+            files += [
+                output_dir / "stats" / "CpuStatsViewer__resource_monitor_batch_0_0.html",
+                output_dir / "stats" / "DiskStatsViewer__resource_monitor_batch_0_0.html",
+                output_dir / "stats" / "MemoryStatsViewer__resource_monitor_batch_0_0.html",
+                output_dir / "stats" / "NetworkStatsViewer__resource_monitor_batch_0_0.html",
+                output_dir / "stats" / "ProcessStatsViewer__cpu_percent.html",
+                output_dir / "stats" / "ProcessStatsViewer__rss.html",
+            ]
+        for filename in files:
+            assert filename.exists()
+        con = sqlite3.connect(db_file)
+        cur = con.cursor()
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        tables = {x[0] for x in cur.fetchall()}
+        assert not {"cpu", "disk", "memory", "network", "process"}.difference(tables)
+        for table in tables:
+            cur.execute(f"SELECT * from {table}")
+            data = cur.fetchall()
+            assert data
